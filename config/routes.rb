6f3626b59df56e1aa628274aaef553a7fa250633
YUTA <sawamura1009@gmail.com>
2018-03-03 23:19:31 +0900

    Add  グループ作成機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages' => 'messages#index'
   root :to => 'messages#index'
-  resource :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
