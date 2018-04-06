c7196e1d6906f20362a70c4e3c2dfff5d1a12ecf
YUTA <sawamura1009@gmail.com>
2018-04-06 19:25:42 +0900

    メッセージ機能　非同期

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,42 @@
+$(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.data}
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <div class="lower-message__content">
+                      ${message.content}
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-messages').append(html)
+      $('#message_content').val('')
+      $(".chat-bottom__box").attr('disabled', false)
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
