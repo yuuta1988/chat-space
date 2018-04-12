95bc3d7e11574bf54d67ac910aceb0e8013b6ac5
YUTA <sawamura1009@gmail.com>
2018-04-12 17:04:43 +0900

    メッセージ送信機能の非同期通信

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,17 +7,6 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
   end
 
-  # def create
-  #   @message = @group.messages.new(message_params)
-  #   if @message.save
-  #     redirect_to group_messages_path(@group)
-  #   else
-  #     @messages = @group.messages.includes(:user)
-  #     flash.now[:alert] = "メッセージを入力してください"
-  #     render :index
-  #   end
-  # end
-
   def create
     @message = @group.messages.new(message_params)
     if @message.save
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.javascripts false
       g.helper false
