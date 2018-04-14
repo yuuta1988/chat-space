cbba6f13bb872c8591f8fdb8613dcc2c7c07285a
YUTA <sawamura1009@gmail.com>
2018-04-14 20:21:26 +0900

    keyup　値取得

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,15 @@ class GroupsController < ApplicationController
     @group.users << current_user
   end
 
+  # def create
+  #   @group = Group.new(group_params)
+  #   if @group.save
+  #     redirect_to root_path, notice: "グループを作成しました"
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def create
     @group = Group.new(group_params)
     if @group.save
