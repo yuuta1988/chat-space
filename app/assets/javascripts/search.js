5c5d7f9584687e2f80200427776d7d4eea550943
YUTA <sawamura1009@gmail.com>
2018-04-16 21:15:38 +0900

    controller修正、変数html

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,13 @@
 $(function(){
+
+  function appendUser(user){
+    var html = `<div class='chat-group-user clearfix>
+                  <p class="chat-group-user">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
+                </div>`
+    return html;
+  }
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
 
@@ -10,6 +19,10 @@ $(function(){
     })
 
     .done(function(users){
+      console.log(users);
+      var html = appendUser(users)
+      console.log(html);
+
     })
   });
 });
