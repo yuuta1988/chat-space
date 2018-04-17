0076b463b492c26035a19fd9129c6358ac28714d
YUTA <sawamura1009@gmail.com>
2018-04-17 21:32:09 +0900

    変数htmlをビューに表示

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,9 +1,9 @@
 $(function(){
 
   function appendUser(user){
-    var html = `<div class='chat-group-user clearfix>
-                  <p class="chat-group-user">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     return html;
   }
@@ -17,12 +17,20 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
-
     .done(function(users){
-      console.log(users);
       var html = appendUser(users)
-      console.log(html);
-
+      $("#chat-group-users").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          var html = appendUser(user);
+          $('#chat-group-users').append(html);
+        })
+      } else {
+        alert("一致するユーザーはいません");
+      }
     })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    });
   });
 });
