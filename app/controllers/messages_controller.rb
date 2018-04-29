71124d70ffd82a00c35dd0a44d0144dbe01b76f2
YUTA <sawamura1009@gmail.com>
2018-04-29 20:01:56 +0900

    change index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
