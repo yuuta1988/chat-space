5d979d94e9d20853c6562520c7097dab29ea82e6
YUTA <sawamura1009@gmail.com>
2018-05-03 20:02:10 +0900

    修正
    アルゴリズム変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
     respond_to do |format|
       format.html
-      format.json
+      format.json { @newmsgs = @messages.where("id > ? ", params[:id]) }
     end
   end
 
